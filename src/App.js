160366b3eda37a5933ae9f1073b7ea2254a8a091
saad0saade008 <saad0saade008@gmail.com>
2025-02-23 12:47:15 +0100

    Description change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          DevOps.
+          Custom_Deplyment.
         </p>
         <a
           className="App-link"
