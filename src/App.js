c8ff0151989f5b0e715168d83aef2a3c1752f46a
saad0saade008 <saad0saade008@gmail.com>
2025-02-25 13:17:09 +0100

    new key 25 feb

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Custom_Deplyment.
+          25_feb.
         </p>
         <a
           className="App-link"
